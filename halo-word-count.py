f8ff45d25db78e484f9492ce500cb426e98ec001
Xinyuan Zhang <zhangxinyuan1995@gmail.com>
2016-12-20 19:45:28 +0800

    finish assignment

--- halo-word-count.py
+++ halo-word-count.py
@@ -3,13 +3,56 @@
 import re
 import collections
 
+import string  
+import sys  
+
 expected = ['the', 'and', 'to', 'of', 'i', 'you', 'a', 'my', 'hamlet', 'in']
 
 def count_words():
     # TODO Please implement code here to analyze the hamlet.txt file and 
     # return an array the 10 most frequently found words in descending order of frequency.
     # Strip punctuation and make your comparisons case-insensitive.
-    raise Exception('TODO')
+    fname = "hamlet.txt"  
+      
+    try:  
+        text = open(fname,'r').read()  
+        text = string.lower(text)  
+    except:  
+        print "\nfile does not exist or I/O error"  
+        sys.exit()  
+    #ignore all punctuation
+    for c in string.punctuation:  
+        text=text.replace(c," ")
+
+    #split into each word
+    words = string.split(text)
+
+    counts = {}  
+    #count freq of word using dict
+    for w in words:  
+        counts[w] = counts.get(w,0) + 1  
+    
+    #sort by value using comparator
+    items = counts.items()  
+    items.sort(compareItems)  
+    
+    #construct result
+    res = []
+    for i in range(10):  
+        #print items[i]
+        res.append(items[i][0])
+
+    return res  
+
+#comparator order by freq in descending order
+def compareItems((w1,c1), (w2,c2)):  
+	if c1 > c2:  
+	    return -1  
+	elif c1 == c2:  
+	    return cmp(w1, w2)  
+	else:  
+	    return 1  
+   
 
 if __name__ == '__main__':
     print('Most Frequent Words...')
